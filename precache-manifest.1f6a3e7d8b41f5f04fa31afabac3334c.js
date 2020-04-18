b1800679dd8d33c333416e6baa18d48ba740eea3
srijit-su <srijit.v9799@gmail.com>
2020-04-18 15:39:56 +0530

    Updates

---
+++ precache-manifest.1f6a3e7d8b41f5f04fa31afabac3334c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1391bccb762c4a9422a96bb7f1b9a3",
+    "url": "/COVID_19/index.html"
+  },
+  {
+    "revision": "1cdab2584bb04046db13",
+    "url": "/COVID_19/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "9d3e0d9fd5dd009d156c",
+    "url": "/COVID_19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1cdab2584bb04046db13",
+    "url": "/COVID_19/static/js/2.3966ea89.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/COVID_19/static/js/2.3966ea89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3e0d9fd5dd009d156c",
+    "url": "/COVID_19/static/js/main.8404ee9a.chunk.js"
+  },
+  {
+    "revision": "b0b922ca8f6988379e6f",
+    "url": "/COVID_19/static/js/runtime-main.ed9ee709.js"
+  }
+]);
